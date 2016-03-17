cc472d276d00afa403d72c21a85f165a3c832228
jacobsintzel <jacob.sintzel@gmail.com>
2016-03-17 11:08:33 -0400

    nav

--- js/getNav.js
+++ js/getNav.js
@@ -1,4 +1,4 @@
-$.get("http://andersonguelph.github.io/partials/nav.html", function(data){
+$.get("http://jacobsintzel.github.io/partials/nav.html", function(data){
 
 $(document).ready(function(){
 $("container").prepend(data);
