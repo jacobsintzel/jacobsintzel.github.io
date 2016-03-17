de5a4495a00900ecaa24ceb607ac6ad9e4a2a860
jacobsintzel <jacob.sintzel@gmail.com>
2016-03-17 13:43:15 -0400

    footer

--- js/getFooter.js
+++ js/getFooter.js
@@ -1,12 +1,12 @@
-$.get("http://jacobsintzel.github.io/partials/nav.html", function(data) {
+$.get("http://jacobsintzel.github.io/partials/footer.html", function(data) {
 
   $(document).ready(function() {
 	//check if this document has .container
-	var container = $(".container");
+	var footer = $(".footer");
 	//if so add nav.html contents to the top
-	if (container) {
-  	container.prepend(data)
-  	container.fadeIn(600);
+	if (footer) {
+  	footer.append(data)
+  	footer.fadeIn(600);
 	}
   });
 });
