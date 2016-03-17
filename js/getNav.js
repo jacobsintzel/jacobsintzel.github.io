f948ffcb286cb1663696f3638b650e6b3b1c5697
jacobsintzel <jacob.sintzel@gmail.com>
2016-03-17 12:00:25 -0400

    nav

--- js/getNav.js
+++ js/getNav.js
@@ -6,7 +6,7 @@ $.get("http://jacobsintzel.github.io/partials/nav.html", function(data) {
 	//if so add nav.html contents to the top
 	if (container) {
   	container.prepend(data)
-  	//container.fadeIn();
+  	container.fadeIn();
 	}
   });
 });
