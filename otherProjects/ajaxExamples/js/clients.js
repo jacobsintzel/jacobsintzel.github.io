947676a8ad63b46549d832d594c0d43a577219e1
jacobsintzel <jacob.sintzel@gmail.com>
2016-03-17 16:26:18 -0400

    table

--- otherProjects/ajaxExamples/js/clients.js
+++ otherProjects/ajaxExamples/js/clients.js
@@ -1,7 +1,20 @@
-$(document).ready(function(){
-$("#getClients").on("click", function(){
+$(document).ready(function() {
+  $("#getClients").on("click", function() {
 
-alert("working");
+    var link = "http://jacobsintzel.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json";
 
-});
-});
+    $.getJSON(link, function(data) {
+      var table = "<table class='table table-hover table-striped'>" +
+        "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
+        .each(data, function(index, item) {
+        table +="<tr>" +
+        "<td>" + item.name +"</td>"
+        "<td>" + item.email + "</td>" +
+        "<td>" + item.company + "</td>" +
+        "</tr>";
+        }); //each
+        table += "</table>"
+        $("#data").append(item.name)//appends data to div
+    }); //getJSON
+  }); //click event
+}); //document ready
