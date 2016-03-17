6bd3bfb6ebbcaf868318e60bd133d21c916641ff
jacobsintzel <jacob.sintzel@gmail.com>
2016-03-17 13:48:42 -0400

    footer

--- js/getNav.js
+++ js/getNav.js
@@ -10,16 +10,3 @@ $.get("http://jacobsintzel.github.io/partials/nav.html", function(data) {
 	}
   });
 });
-
-$.get("http://jacobsintzel.github.io/partials/footer.html", function(data) {
-
-  $(document).ready(function() {
-	//check if this document has .container
-	var footer = $(".footer");
-	//if so add footer.html contents to the bottom
-	if (footer) {
-  	footer.append(data)
-  	footer.fadeIn(600);
-	}
-  });
-});
