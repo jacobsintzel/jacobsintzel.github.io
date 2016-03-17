6f6f6c8a932ba7abd14b585cd60cd5300a2cce52
jacobsintzel <jacob.sintzel@gmail.com>
2016-03-17 13:45:57 -0400

    footer

--- js/getFooter.js
+++ js/getFooter.js
@@ -3,7 +3,7 @@ $.get("http://jacobsintzel.github.io/partials/footer.html", function(data) {
   $(document).ready(function() {
 	//check if this document has .container
 	var footer = $(".footer");
-	//if so add nav.html contents to the top
+	//if so add footer.html contents to the bottom
 	if (footer) {
   	footer.append(data)
   	footer.fadeIn(600);
