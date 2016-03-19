9688d81961ea404850bc3c369648de58ff2653e3
jacobsintzel <jacob.sintzel@gmail.com>
2016-03-19 14:38:43 -0400

    list

--- otherProjects/ajaxExamples/js/events.js
+++ otherProjects/ajaxExamples/js/events.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
       var list = "<ul class='list-group'>";
         $.each(data, function(index, value) {
         list +=
-        "<li class='list-group-item list-group-item-success'>" + value.location +"</li>" +
+        "<li class='list-group-item list-group-item-success'>"+"<h4 class='list-group-item-heading'>"+ value.location +"</h4>"+"</li>" +
         "<li class='list-group-item list-group-item-info'>" + value.date + "</li>" +
         "<li class='list-group-item list-group-item-warning'>" + value.time + "</li>";
         }); //each
