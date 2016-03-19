291483ce3d39ba769ed6ff704ce9fe5aea21e80a
jacobsintzel <jacob.sintzel@gmail.com>
2016-03-19 14:31:55 -0400

    list

--- otherProjects/ajaxExamples/js/events.js
+++ otherProjects/ajaxExamples/js/events.js
@@ -4,17 +4,15 @@ $(document).ready(function() {
     var link="http://jacobsintzel.github.io/otherProjects/ajaxExamples/jsonDatabase/events.json";
 
     $.getJSON(link, function(data) {
-      var table = "<table class='table table-hover table-striped'>" +
-        "<tr><th>Location</th><th>Date</th><th>Time</th></tr>";
+      var list = "<ul class='list-group'>";
         $.each(data, function(index, value) {
-        table +="<tr>" +
-        "<td>" + value.location +"</td>" +
-        "<td>" + value.date + "</td>" +
-        "<td>" + value.time + "</td>" +
-        "</tr>";
+        list +=
+        "<li class='list-group-item'>" + value.location +"</li>" +
+        "<li class='list-group-item'>" + value.date + "</li>" +
+        "<li class='list-group-item'>" + value.time + "</li>";
         }); //each
-        table += "</table>"
-        $("#data").append(table);//appends data to div
+        list += "</ul>"
+        $("#data").append(list);//appends data to div
     }) //getJSON
   }) //click event
 }); //document ready
