896140ca3ca0320fd8ddc575d65f2911eab37254
J Sintzel <jacob.sintzel@gmail.com>
2016-09-30 18:35:11 -0400

    update

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    //name and line fade 
+    //name and line fade
     $('#name').hide().fadeIn(800);
     $('.line').animate({
         width: '+=99%'
@@ -8,7 +8,7 @@ $(document).ready(function() {
     $('.mobileNav').hide().fadeIn(800);
 
     //click buttons
-    $('a:not(".externalLink")').on('click', function(e) {
+    $('a:not(".externalLink")').on('dblclick', function(e) {
         e.preventDefault();
         var url = this.href;
         $('#container').empty();
@@ -18,25 +18,42 @@ $(document).ready(function() {
     //change the navigation for bigger screens
     function reSize() {
         $('.different').on('mouseover resize', function() {
-            if ($(window).width() > 767) {
                 $(this).css({
-                    "opacity": "0.8",
-                    "filter": "alpha(opacity=80)"
+                    "opacity": "0.9",
+                    "filter": "alpha(opacity=90)"
                 }); //opacity change
+            if ($(window).width() > 767) {
                 $(this).find('.mobileNavIconsIndex').css({
                     'display': 'none'
                 }); //hide icon
-                var $child = $(this).find('.bigScreenNavText'); //store text 
+                var $child = $(this).find('.bigScreenNavText'); //store text
+                if($(window).width() > 896){
                 $child.css({
                     'display': 'block',
-                    'font-size': '40px'
-                }); //change navText display
+                    'font-size': '50px',
+                    'font-weight':'900',
+                    'text-decoration':'underline'
+                });//change navText display
+                var retrieveAttr=this.id.toUpperCase();//retrieve lower case id on .different & make uppercase
+                var thisImage="url('../images/"+retrieveAttr+".jpg')"//variable for displaying specific image
+                $(this).css({'background-image': thisImage, 'background-repeat':'no-repeat', 'background-position':'center', 'background-size':'cover'})
+                }else{
+                  $child.css({
+                    'display': 'block',
+                    'font-size': '30px',
+                    'font-weight':'900',
+                    'text-decoration':'underline'
+                    }); //change navText display
+                    var retrieveAttr=this.id.toUpperCase();//retrieve lower case id on .different & make uppercase
+                    var thisImage="url('../images/"+retrieveAttr+".jpg')"//variable for displaying specific image
+                    $(this).css({'background-image': thisImage, 'background-repeat':'no-repeat', 'background-position':'center', 'background-size':'cover'});
+                }
             }
         })
         $('.different').on('mouseout resize', function() {
             $(this).css({
-                "opacity": "1",
-                "filter": "alpha(opacity=100)"
+                    "opacity": "1",
+                    "filter": "alpha(opacity=100)"
             }); //opacity change
             $(this).find('.mobileNavIconsIndex').css({
                 'display': 'block'
@@ -46,6 +63,7 @@ $(document).ready(function() {
                 'display': 'none',
                 'font-size': '0px'
             }); //change navText display
+            $(this).css({'background-image': ''})
         })
     }
 
